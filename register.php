8a6d3d8e761c568f00e89e005212728ee9f18593
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-07-31 18:08:13 +0200

    start up

---
+++ register.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once (__DIR__ . "/classes/User.php");
+
+
+
+?>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registreren</title>
+</head>
+<body>
+
+<div class="aanmelden">
+      
+        
+        
+        <div id="form_aanmelden">
+			<form action="" method="post">
+				
+				<div class="formfield">
+					<label for="username">Kies een username:</label>
+					<br>
+					<input type="text" id="username" name="username">
+				</div>
+				<br>
+
+				<div class="formfield">
+					<label for="email">Emailadres:</label>
+					<br>
+					<input type="text" id="email" name="email">
+				</div>
+                <br>
+
+			    <div class="formfield">
+					<label for="wachtwoord">Kies je wachtwoord:</label>
+					<br>
+					<input type="password" id="wachtwoord" name="wachtwoord">
+				</div> 
+                <br>
+                
+				<div class="formfield">
+					<label for="wachtwoord_herhaling">Herhaal wachtwoord:</label>
+					<br>
+					<input type="password" id="wachtwoordHerhaling" name="wachtwoordHerhaling">
+				</div>
+                <br>
+				<?php if (isset($error)) : ?>
+    			<div class="error" >
+					<?php echo $error ?>
+				</div>
+				<?php endif; ?>
+				<div class="formfield_submit">
+					<input type="submit" value="registreren" class="btn-aanmelden">	
+				</div>
+
+               
+                
+			</form>
+		 </div>
+	</div>
+    
+</body>
+</html>
---
+++ settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+const SETTINGS = [
+    "db" => [
+        "user"      => "root",
+        "password"  => "",
+        "host"      => "localhost",
+        "db"        => "currencyapp",
+        "port"      => 3306
+    ]
+    ];
