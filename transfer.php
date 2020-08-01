c702b8c230869da1a61f0925294c526d7a7ea6f0
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-01 10:46:54 +0200

    update transfer

--- transfer.php
+++ transfer.php
@@ -11,9 +11,25 @@ include_once (__DIR__ . "/classes/Transfer.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Transfer</title>
 </head>
 <body>
+
+<form method="GET" action="">
+        <div>
+            <p>Zoek een gebruiker</p>
+
+            <div class="form">
+                <label for="name"><b>Naam</b></label>
+                <input class="form-control" type="text" name="searchField" placeholder="Naam" id="searchName" autocomplete="off">
+                <div id="zoekbalk"></div>
+            </div>
+
+            <div class="form">
+                <input class="btn border search-name-btn" type="submit" value="Naam zoeken" name='searchName'>
+            </div>
+    </form>
+
     
 </body>
 </html>
