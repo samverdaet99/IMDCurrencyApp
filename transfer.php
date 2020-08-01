74b4fb28a49f0800e97c4d776c36188a43276d9b
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-01 10:31:34 +0200

    transacties aanmaken

---
+++ transfer.php
@@ -0,0 +1,19 @@
+<?php
+include_once (__DIR__ . "/classes/User.php");
+include_once (__DIR__ . "/classes/Transfer.php");
+
+?>
+
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
