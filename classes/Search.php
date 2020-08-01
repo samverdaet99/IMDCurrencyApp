680519daa79757dec8e0ba7f0096687c6026f7bb
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-01 11:31:16 +0200

    Search

---
+++ classes/Search.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once (__DIR__ . "/Db.php");
+
+$output="";
+
+if (isset($_POST['search'])){
+    $searchq = $_POST['search'];
+    $searchq = preg_replace("#0-9a-z#i","",$searchq);
+
+    //$query = mysql_query("SELECT * FROM members WHERE username LIKE '%$searchq%'") or die("could not seacrh");
+
+    $conn = Db::getConnection();
+    $query = $conn->prepare("SELECT * FROM members WHERE username LIKE '%$searchq%'") or die("could not seacrh");
+ 
+
+
+    $count = mysqli_num_rows($query);
+    if ($count == 0){
+        $output = "there was no search results!";
+    } else {
+        while($row = mysql_fetch_array($query)){
+            $uName = $row['username'];
+            $id = $row['id'];
+
+            $output .= '<div>' .$uName. '</div>';
+        }
+    }
+
+}
+
+?>
--- transfer.php
+++ transfer.php
@@ -1,7 +1,6 @@
 <?php
 include_once (__DIR__ . "/classes/User.php");
-include_once (__DIR__ . "/classes/Transfer.php");
-
+include_once (__DIR__ . "/classes/Transaction.php");
 ?>
 
 
