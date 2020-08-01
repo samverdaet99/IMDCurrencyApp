81f784053d03072b89e445b9840c07a0956dc443
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-01 08:57:45 +0200

    logout

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once(__DIR__ . '/classes/User.php');
+include_once(__DIR__ . '/classes/logout.php');
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMDcurrency App</title>
+</head>
+<body>
+
+<h1>Aanmelden gelukt!</h1>
+
+<a href="logout.php">
+Uitloggen</a>
+    
+</body>
+</html>
