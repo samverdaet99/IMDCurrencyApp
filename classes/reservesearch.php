454209f30ce06ed1fc99a9176973b2d0b4c3f9dd
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-02 17:17:31 +0200

    andere poging Search
    Mysqli -> mag niet gebruikt worden -> andere oplossing

--- classes/reservesearch.php
+++ classes/reservesearch.php
@@ -85,3 +85,47 @@ $(document).ready(function(){
 </script>
 
 -->
+
+
+
+
+
+
+-------------------------------------
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+if(isset($_REQUEST["term"])){
+    // Prepare a select statement
+    $sql = "SELECT * FROM users WHERE username LIKE ?";
+    global $link;
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_term);
+        
+        // Set parameters
+        $param_term = $_REQUEST["term"] . '%';
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+            
+            // Check number of rows in the result set
+            if(mysqli_num_rows($result) > 0){
+                // Fetch result rows as an associative array
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    echo "<p>" . $row["name"] . "</p>";
+                }
+            } else{
+                echo "<p>Geen matches gevonden</p>";
+            }
+        } else{
+            echo "ERROR: er ging iets fout $sql. " . mysqli_error($link);
+        }
+    }
+     
+
+}
