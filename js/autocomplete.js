c4f55fc0427635d2c7330b71087c1c61cba2a88f
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-03 10:14:12 +0200

    Update autocomplete.js

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,9 +1,28 @@
 
 
 var resultSearch = document.querySelector("#resultSearch");
+resultSearch.style.display= "none";
 
 document.querySelector("#searchUser").addEventListener("keyup", event => {
     let input = document.querySelector("#searchUser").nodeValue;
     let searchResults = new searchResults();
     searchResults.append("text", input);
-})
+
+    fetch('/ajax/autocomplete.php', {
+        method: "POST",
+        body: searchResults })
+
+    .then(response => response.json())
+        .then(result => {  
+            resultSearch.innerHTML=" ";
+            resultSearch.style.display = "block";
+
+            result.body.forEach(element => {
+                let result = document.createElement('a');
+                result.innerHTML = element.username;
+                resultSearch.appendChild(result);
+    });
+}) .catch(error => {
+    console.log("Error", error);
+        });
+});
