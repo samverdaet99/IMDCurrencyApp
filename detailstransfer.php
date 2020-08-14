64a5909d627702fe5025de86690e686a54e9f3ee
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-14 13:47:51 +0200

    details en transacties IN ORDE

--- detailstransfer.php
+++ detailstransfer.php
@@ -1,7 +1,8 @@
 <?php
-//include_once (__DIR__ . "/classes/User.php");
+include_once (__DIR__ . "/classes/User.php");
 include_once (__DIR__ . "/classes/Transaction.php");
-
+ 
+  
 //$transfer = new Transaction();
 session_start();
 
@@ -19,30 +20,46 @@ session_start();
     $emptymessage = "Nog geen transacties";
     }
 
-?>
 
+//if(isset($_SESSION['transfer'])){
+  //if (!empty($_POST)) {
+
+   // $transfer = new Transaction();
+
+    //$allTransfers = $transfer->getTransfers($_SESSION['transfer']);
+
+    //if ($allTransfers == null)
+    //{
+    //$emptymessage = "Nog geen transacties";
+    //}
+   
+  //} else {
+    //header("Location: alletransacties.php");
+  //}
+
+
+
+  ?>
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <title>IMDCurrency - details transfer</title>
+    <title>IMDCurrency - Details</title>
 </head>
 <body>
 
 
-
-
 <div id="terug"><a href="alletransacties.php">&#8592;</a></div>
 
+
 <div id="paginatitel">
-	<h1>Details transactie</h1>
-	
+	<h1>Details</h1>
+	<p>Hieronder vindt u een gedetailleerde weergave van de transacties.</p>
 </div>
 
 <section id="kader_details">
-
 
 <div id="details_kader">
 
@@ -51,11 +68,20 @@ session_start();
     <?php endif; ?>   
 
 
-<?php foreach ($allTransfers as $allTranser) :?>
+    <?php
 
 
+for($i=0, $count = count($transactieOntvanger);$i<$count;$i++):
+          $transactieOntvangers = $transactieOntvanger[$i];
+          $transactieVerzenders = $transactieVerzender[$i];
+
+      
+
+if (($transactieOntvangers['user_ontvanger']  == $_SESSION['userid']) || ($transactieVerzenders['user_verzender'] == $_SESSION['userid'])){
+
+  ?>
   <div id="details_datum">
-  <p> Uitvoerdatum: <br><?php echo $allTranser['datum'];?> </p>
+  <p> Uitvoerdatum: <br><?php echo $transactieVerzenders['datum']?> </p>
   </div>
 
 
@@ -63,32 +89,63 @@ session_start();
 
   <div id="details_gegevens">
 
-  <p>Verzender: <br><?php echo $allTranser['user_verzender'];?></p>
-  <p>Ontvanger: <br><?php echo $allTranser['user_ontvanger'];?> </p>
+ 
+        
+            <?php 
+            {
+                
+                 ?>
 
+                    <?php echo  " verzender: ";
+                      echo $transactieVerzenders['username'];
+                      
+                          
+                    
+           } 
+     {                  echo  "<br> ontvanger: ";
+                         echo  $transactieOntvangers['username']; ; ?>
+
+
+                <?php           
+            } 
+           ?>
+    
+       
+  
+
+    <div id="details_beschrijving">
+  <p> Beschrijving van de transactie: <br><?php echo $transactieVerzenders['description'];?> </p>
+  </div>
 
 
   <div id="details_bedrag">
-  <p> Tokens: <br><?php echo $allTranser['bedrag'];?> </p>
+  <p> Aantal tokens: <br><?php echo $transactieVerzenders['bedrag']?> </p>
   </div>
+
+
+
+</div>
+
+<?php     
+} else{
+  
+}
+endfor; 
+
+
+?>
+ 
+
+
+  
+  
+
 
 
   
 
-  <div id="details_beschrijving">
-  <p> Beschrijving transactie: <br><?php echo $allTranser['description'];?> </p>
-  </div>
+</section>    
 
-  </div>
- 
-  <?php endforeach; ?>
-
-      </div>
-</div>
-
-
-
-</section>
 
     
 </body>
