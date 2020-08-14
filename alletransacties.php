fe2077fdb89cff7695c8edfb8572e4908487959c
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-14 11:05:10 +0200

    terug andere backup

--- alletransacties.php
+++ alletransacties.php
@@ -3,12 +3,13 @@ include_once (__DIR__ . "/classes/User.php");
 include_once (__DIR__ . "/classes/Transaction.php");
  
   
-//$transfer = new Transaction();
 session_start();
+
 
     $transfer = new Transaction();
     $allTransfers = $transfer->getTransfers();
 
+  // NB: OM USERNAMES TE LATEN ZIEN ZOU IK DEZE FUNCTIE GEBRUIKEN, HET IS MIJ ECHTER NOOIT GELUKT OM DEZE 100% WERKEND TE KRIJGEN, VANDAAR DAT IK HET MET DE ID'S HEB GEDAAN.
     $transactieVerzender = Transaction::transactiesVerzender($transfer);
     $transactieOntvanger = Transaction::transactiesOntvanger($transfer);
 
@@ -19,23 +20,6 @@ session_start();
     {
     $emptymessage = "Nog geen transacties";
     }
-
-
-//if(isset($_SESSION['transfer'])){
-  //if (!empty($_POST)) {
-
-   // $transfer = new Transaction();
-
-    //$allTransfers = $transfer->getTransfers($_SESSION['transfer']);
-
-    //if ($allTransfers == null)
-    //{
-    //$emptymessage = "Nog geen transacties";
-    //}
-   
-  //} else {
-    //header("Location: alletransacties.php");
-  //}
 
 
 
@@ -53,7 +37,6 @@ session_start();
 
 <div id="terug"><a href="index.php">&#8592;</a></div>
 
-
 <div id="paginatitel">
 	<h1>Mijn  transacties</h1>
 	<p>Hieronder vindt u een weergave van de transacties die u reeds hebt uitgevoerd.</p>
@@ -61,81 +44,67 @@ session_start();
 
 <section id="kader_details">
 
+  
+
+
+  
+
 <div id="details_kader">
 
 <?php if(isset($emptymessage)) :?>
         <h2 class="emptyMessage"><?php echo $emptymessage; ?></h2>
     <?php endif; ?>   
 
+    <!--
+      NB: OM USERNAMES TE LATEN ZIEN ZOU IK DEZE FUNCTIE GEBRUIKEN, HET IS MIJ ECHTER NOOIT GELUKT OM DEZE 100% WERKEND TE KRIJGEN, VANDAAR DAT IK HET MET DE ID'S HEB GEDAAN.
 
-    <?php
 
-
-for($i=0, $count = count($transactieOntvanger);$i<$count;$i++):
+      for($i= 1, $count = count($transactieOntvanger);$i<$count;$i++):
           $transactieOntvangers = $transactieOntvanger[$i];
           $transactieVerzenders = $transactieVerzender[$i];
 
       
 
-if (($transactieOntvangers['user_ontvanger']  == $_SESSION['userid']) || ($transactieVerzenders['user_verzender'] == $_SESSION['userid'])){
+      if (($transactieOntvangers['user_ontvanger']  == $_SESSION['userid']) || ($transactieVerzenders['user_verzender'] == $_SESSION['userid'])){
 
-  ?>
+-->
+
+<?php foreach ($allTransfers as $allTranser) :?>
+
   <div id="details_datum">
-  <p> Uitvoerdatum: <br><?php echo $transactieVerzenders['datum']?> </p>
+  <p> Uitvoerdatum: <br><?php echo $allTranser['datum'];?> </p>
   </div>
-
-
-
 
   <div id="details_gegevens">
 
- 
-        
-            <?php 
-            {
-                
-                 ?>
-
-                    <?php echo  " verzender: ";
-                      echo $transactieVerzenders['username'];
-                      
-                          
-                    
-           } 
-     {                  echo  "<br> ontvanger: ";
-                         echo  $transactieOntvangers['username']; ; ?>
-
-
-                <?php           
-            } 
-           ?>
-    
-       
-  
-
+  <p>Verzender: <br><?php echo $allTranser['user_verzender'];?></p>
+  <p>Ontvanger: <br><?php echo $allTranser['user_ontvanger'];?> </p>
 
 
   <div id="details_bedrag">
-  <p> Tokens: <br><?php echo $transactieVerzenders['bedrag']?> </p>
+  <p> Tokens: <br><?php echo $allTranser['bedrag'];?> </p>
   </div>
+  
 
-<div class="btndetails"><a href="detailstransfer.php">Bekijk details</a></div>
+  <div class="btndetails"><a href="detailstransfer.php">Bekijk details</a></div>
 
+  </div>
+ 
+  <?php endforeach; ?>
+
+      </div>
 </div>
 
-<?php     
-} else{
-  
-}
-endfor; 
-
-
-?>
- 
 
 
   
-  
+
+</section>    
+
+
+    
+</body>
+</html>
 
 
 
