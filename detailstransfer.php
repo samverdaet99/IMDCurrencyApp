4281e6b66161368ef2882f9574c8e6a73c69c8f9
samverdaet99 <samverdaet99@users.noreply.github.com>
2020-08-15 16:51:28 +0200

    BACKUP hergebruiken

--- detailstransfer.php
+++ detailstransfer.php
@@ -2,13 +2,12 @@
 include_once (__DIR__ . "/classes/User.php");
 include_once (__DIR__ . "/classes/Transaction.php");
  
-
+  
+//$transfer = new Transaction();
 session_start();
 
     $transfer = new Transaction();
     $allTransfers = $transfer->getTransfers();
-
-    $user = new User();
 
     $transactieVerzender = Transaction::transactiesVerzender($transfer);
     $transactieOntvanger = Transaction::transactiesOntvanger($transfer);
@@ -20,6 +19,22 @@ session_start();
     {
     $emptymessage = "Nog geen transacties";
     }
+
+//if(isset($_SESSION['transfer'])){
+  //if (!empty($_POST)) {
+
+   // $transfer = new Transaction();
+
+    //$allTransfers = $transfer->getTransfers($_SESSION['transfer']);
+
+    //if ($allTransfers == null)
+    //{
+    //$emptymessage = "Nog geen transacties";
+    //}
+   
+  //} else {
+    //header("Location: alletransacties.php");
+  //}
 
 
   ?>
@@ -33,47 +48,90 @@ session_start();
 </head>
 <body>
 
-
 <div id="terug"><a href="alletransacties.php">&#8592;</a></div>
 
-
 <div id="paginatitel">
-	<h1>Details</h1>
-	<p>Hieronder vindt u een gedetailleerde weergave van de transacties.</p>
+  <h1>Details</h1>
+  <p>Hieronder vindt u een gedetailleerde weergave van de transacties.</p>
 </div>
 
 <section id="kader_details">
 
 <div id="details_kader">
 
-<?php
+<?php if(isset($emptymessage)) :?>
+        <h2 class="emptyMessage"><?php echo $emptymessage; ?></h2>
+    <?php endif; ?>   
 
+    <?php
 
-$detWeergeven = Transaction::showDetails($_GET['id']);
-var_dump($detWeergeven);
+for($i=0, $count = count($transactieOntvanger);$i<$count;$i++):
+          $transactieOntvangers = $transactieOntvanger[$i];
+          $transactieVerzenders = $transactieVerzender[$i];
 
+      
 
-?>
+if (($transactieOntvangers['user_ontvanger']  == $_SESSION['userid']) || ($transactieVerzenders['user_verzender'] == $_SESSION['userid'])){
 
-
-<div id="details_bedrag">
-  <p> Tokens: <br><?php echo $detWeergeven['bedrag']?> </p>
+  ?>
+  <div id="details_datum">
+  <p> Uitvoerdatum: <br><?php echo $transactieVerzenders['datum']?> </p>
   </div>
 
-<?php
-
-?>
 
 
+  <div id="details_gegevens">
+
+ 
+        
+            <?php 
+            {
+                
+                 ?>
+
+                    <?php echo  " verzender: ";
+                      echo $transactieVerzenders['username'];
+                      
+                          
+                    
+           } 
+     {                  echo  "<br> ontvanger: ";
+                         echo  $transactieOntvangers['username']; ; ?>
+
+                <?php           
+            } 
+           ?>
+    
+       
+  
+
+    <div id="details_beschrijving">
+  <p> Beschrijving van de transactie: <br><?php echo $transactieVerzenders['description'];?> </p>
+  </div>
+
+  <div id="details_bedrag">
+  <p> Aantal tokens: <br><?php echo $transactieVerzenders['bedrag']?> </p>
+  </div>
 
 
 </div>
 
+<?php     
+} else{
+  
+}
+endfor; 
 
+?>
  
 
-</section>    
+  
+  
 
+
+  
+
+</section>    
 
     
 </body>
